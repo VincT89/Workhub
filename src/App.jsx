b0da82dba24573849214331412e952156a05848f
VincT89 <vincenzotito@icloud.com>
2025-11-02 10:56:07 +0100

    icone, bg, logo caricati

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage.jsx";
+
+// import PublicLayout from "./layouts/PublicLayout.jsx";
+// import LoginPage from "./pages/LoginPage.jsx";
+// import SettingsPage from "./pages/SettingsPage.jsx";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<PublicLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="settings" element={<SettingsPage />} />
-      </Route>
+      {/* <Route path="/" element={<PublicLayout />}> */}
+        <Route path="/" element={<HomePage />} />
+         <Route path="login" element={<LoginPage />} />
+        {/*<Route path="settings" element={<SettingsPage />} /> */}
+      {/* </Route> */}
     </Routes>
   );
 }
