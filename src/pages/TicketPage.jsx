131c5dbc8614dc36ad96f56bef941aa62cf8918c
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-11-08 12:19:10 +0100

    feat: update TicketPage to include BarChart and integrate webUsageState for PieChart

--- src/pages/TicketPage.jsx
+++ src/pages/TicketPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import bgImage from "../assets/bg/bg.jpg";
+
+//import del grafico da React Pie Charts -> Link in caso serva : https://mui.com/x/react-charts/highlighting/#highlighting-series
 import { PieChart } from '@mui/x-charts/PieChart';
-import { desktopOS, valueFormatter } from '../store/webUsageState';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+//tutte le icone 
 import openIcon from "../assets/icons/Open Envelope Clock.png";
 import checkIcon from "../assets/icons/Instagram Check Mark.png";
 import pendingIcon from "../assets/icons/Data Pending.png";
@@ -9,10 +13,14 @@ import errorIcon from "../assets/icons/Error.png";
 import reportIcon from "../assets/icons/Report File.png";
 import listIcon from "../assets/icons/List.png";
 import personalIcon from "../assets/icons/Test Passed.png";
+import webUsageState from "../store/webUsageState";
 
 
 
 const TicketPage = () => {
+
+    const [highlightedItem, setHighLightedItem] = React.useState(null);
+
     return (
         <main className="bg-white w-full min-h-screen relative overflow-hidden">
             {/* Background */}
@@ -24,43 +32,43 @@ const TicketPage = () => {
 
             {/* Overlay effetto vetro che contiene i miei elemnti */}
             <div className="absolute top-[5%] left-[10%] w-[80%] h-[80%] bg-[#fafafa20] backdrop-blur-sm rounded-[25px] border border-solid shadow-lg border-neutral-50/30 p-6">
-                
 
-                    {/* HEADER - Statistiche principali: Ticket Aperti, test-passedTicket Risolti, test-passedTicket in attesa, test-passedTicket urgenti*/}
-                    <section className=" grid grid-cols-4 gap-4 mb-6">
-                        <div className="flex items-center justify-between bg-white/60 backdrop-blur-sm rounded-xl px-4 py-3 shadow">
-                            <span className="flex items-center gap-2 font-semibold text-blue-900">
-                                <img src={openIcon} alt="ticket aperti" className="w-5 h-5 object-contain" />
-                                Ticket Aperti
-                            </span>
-                            <span className="font-bold text-lg text-blue-800">0</span>
-                        </div>
 
-                        <div className="flex items-center justify-between bg-white/60 backdrop-blur-sm rounded-xl px-4 py-3 shadow">
-                            <span className="flex items-center gap-2 font-semibold text-blue-900">
-                                <img src={checkIcon} alt="check icon" className="w-5 h-5 object-contain" />
-                                Ticket Risolti
-                            </span>
-                            <span className="font-bold text-lg text-blue-800">11</span>
-                        </div>
+                {/* HEADER - Statistiche principali: Ticket Aperti, test-passedTicket Risolti, test-passedTicket in attesa, test-passedTicket urgenti*/}
+                <section className=" grid grid-cols-4 gap-4 mb-6">
+                    <div className="flex items-center justify-between bg-white/60 backdrop-blur-sm rounded-xl px-4 py-3 shadow">
+                        <span className="flex items-center gap-2 font-semibold text-blue-900">
+                            <img src={openIcon} alt="ticket aperti" className="w-5 h-5 object-contain" />
+                            Ticket Aperti
+                        </span>
+                        <span className="font-bold text-lg text-blue-800">0</span>
+                    </div>
 
-                        <div className="flex items-center justify-between bg-white/60 backdrop-blur-sm rounded-xl px-4 py-3 shadow">
-                            <span className="flex items-center gap-2 font-semibold text-blue-900">
-                                <img src={pendingIcon} alt="pending icon" className="w-5 h-5 object-contain" />
-                                Ticket in attesa
-                            </span>
-                            <span className="font-bold text-lg text-blue-800">22</span>
-                        </div>
+                    <div className="flex items-center justify-between bg-white/60 backdrop-blur-sm rounded-xl px-4 py-3 shadow">
+                        <span className="flex items-center gap-2 font-semibold text-blue-900">
+                            <img src={checkIcon} alt="check icon" className="w-5 h-5 object-contain" />
+                            Ticket Risolti
+                        </span>
+                        <span className="font-bold text-lg text-blue-800">11</span>
+                    </div>
 
-                        <div className="flex items-center justify-between bg-white/60 backdrop-blur-sm rounded-xl px-4 py-3 shadow">
-                            <span className="flex items-center gap-2 font-semibold text-blue-900">
-                                <img src={errorIcon} alt="error icon" className="w-5 h-5 object-contain" />
-                                Ticket urgenti
-                            </span>
-                            <span className="font-bold text-lg text-blue-800">3</span>
-                        </div>
-                    </section>
-                    <div className="flex flex-col items-center justify-center w-full h-full">
+                    <div className="flex items-center justify-between bg-white/60 backdrop-blur-sm rounded-xl px-4 py-3 shadow">
+                        <span className="flex items-center gap-2 font-semibold text-blue-900">
+                            <img src={pendingIcon} alt="pending icon" className="w-5 h-5 object-contain" />
+                            Ticket in attesa
+                        </span>
+                        <span className="font-bold text-lg text-blue-800">22</span>
+                    </div>
+
+                    <div className="flex items-center justify-between bg-white/60 backdrop-blur-sm rounded-xl px-4 py-3 shadow">
+                        <span className="flex items-center gap-2 font-semibold text-blue-900">
+                            <img src={errorIcon} alt="error icon" className="w-5 h-5 object-contain" />
+                            Ticket urgenti
+                        </span>
+                        <span className="font-bold text-lg text-blue-800">3</span>
+                    </div>
+                </section>
+                <div className="flex flex-col items-center justify-center w-full h-full">
 
                     {/* CONTENUTO PRINCIPALE: report Ticket(con futuro grafico), ticket recenti, risposte persdonali*/}
 
@@ -76,11 +84,13 @@ const TicketPage = () => {
                                 {/*     al momento ho messo un cerchio colorato, il grafico come lo faremo? 
                                     <div className="w-60 h-60 bg-gradient-to-br from-blue-400 to-pink-400 rounded-full" />
                             */}
-                                <div className="flex-1 flex items-center justify-center">
+                                <div className="w-90 h-90 flex-1 flex items-center justify-center">
                                     <PieChart
-                                        series={[{ data: desktopOS, valueFormatter }]}
-                                        width={180}
-                                        height={180}
+                                        {...webUsageState}
+                                        highlightedItem={highlightedItem}
+                                        onHighlightChange={setHighLightedItem}
+                                        width={320}
+                                        height={320}
                                     />
                                 </div>
                             </div>
