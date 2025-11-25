163b83c2421227a891accffd74e4981d28580b56
VincT89 <vincenzotito@icloud.com>
2025-11-25 16:02:11 +0100

    models ok

---
+++ server/db/models/UserLeave.js
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+
+const UserLeaveSchema = new Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: "user",
+			required: true,
+		},
+		vacationHours: {
+			type: Number,
+			required: true,
+		},
+		leaveHours: {
+			type: String,
+			required: true,
+		},
+		requestedHours: {
+			type: [
+				{
+					year: Number,
+					hours: Number,
+					mode: {
+						type: String,
+						enum: ["vacation", "leave"],
+					},
+					status: {
+						type: String,
+						enum: ["approved", "pending", "denied"],
+						default: "pending",
+					},
+				},
+			],
+		},
+	},
+	{ strict: true, timestamps: true, versionKey: false }
+);
+
+const UserLeaveModel = model("UserLeave", UserLeaveSchema);
+
+export default UserLeaveModel;
