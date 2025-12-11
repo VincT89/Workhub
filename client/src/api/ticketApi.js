0ed50d4f339853be8be122a854cfeba2b8cd9800
Shala21 <119969733+Shala21@users.noreply.github.com>
2025-12-11 10:15:26 +0100

    Initialize ticketApi and ticketSlice files for CRUD operations

---
+++ client/src/api/ticketApi.js
@@ -0,0 +1,2 @@
+//creare le funzioni per pert gestire le fetch CRUD dei ticket per richiamarle dal backend.
+import { API_URL } from "../config/api";
---
+++ client/src/store/feature/ticketSlice.js
@@ -0,0 +1,4 @@
+// questo pag richiama le funzioni per gestire le CRUD nel file ticket api (ticketApi) in piu salva tutto in redux. Usa asyncThunk per azioni asincrone
+
+//import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+//import { fetchTickets, createTicket, updateTicket, deleteTicket } from "../../api/ticketApi";
